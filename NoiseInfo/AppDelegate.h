971dc2010344f2a18dd5b0fadc832f1462b37029
Leohongyi <wenhualiu.liu@gmail.com>
2015-11-02 23:17:37 -0500

    Initial Commit

---
+++ NoiseInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoiseInfo
+//
+//  Created by apple on 11/2/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoiseInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoiseInfo
+//
+//  Created by apple on 11/2/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
